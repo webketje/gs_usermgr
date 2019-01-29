2c08ffbdef5ea6dc65a093ea259c2760eedd92dc
webketje <kevin.van.lierde@gmail.com>
2019-01-29 18:37:30 +0100

    Add de_DE & fr_FR languages

---
+++ usermgr/lang/fr_FR.php
@@ -0,0 +1,8 @@
+<?php /* this file is part of the GS UserMgr plugin package, Copyright (c) 2017  Kevin Van Lierde <kevin.van.lierde@gmail.com> */
+
+	$i18n = array(
+		'plugin_name' => 'GS UserManager',
+		'plugin_desc' => 'Gestion d\'utilisateurs moderne, comprenant des permissions, groupes, et règles d\'acces',
+		'permission_denied_title' => 'Non authorisé',
+		'permission_denied_content' => '<p>Vous n\'avez pas accès à cette page.</p>'
+	);
